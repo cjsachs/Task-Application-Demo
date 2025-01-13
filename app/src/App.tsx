387254540058a6a314cf0707601f6a8158ce8d39
Christian Sachs <byaskew3@gmail.com>
2025-01-13 09:59:11 -0700

    added protected routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Navigate } from 'react-router';
 import { Route, Routes } from 'react-router';
 import TaskContextProvider from './context/TaskContext';
 import TaskDashboard from './components/TaskDashboard';
@@ -6,6 +6,15 @@ import TaskDetails from './components/TaskDetails';
 import TaskForm from './components/TaskForm';
 import AuthPage from './components/AuthPage';
 import Navigation from './components/Navigation';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+
+// Route Guard Component
+const ProtectedRoute = ({ component }: { component: React.ComponentType}) => {
+  const Component = withAuthenticationRequired(component, {
+    onRedirecting: () => <div>Loading...</div>
+  })
+  return <Component/>
+}
 
 const App = () => {
   return (
@@ -14,10 +23,11 @@ const App = () => {
         <BrowserRouter>
           <Navigation />
           <Routes>
-            <Route path="/" element={<TaskDashboard />} />
-            <Route path="/task/:id" element={<TaskDetails />} />
-            <Route path="/task/new" element={<TaskForm />} />
+            <Route path="/" element={<ProtectedRoute component={TaskDashboard}/>} />
+            <Route path="/task/:id" element={<ProtectedRoute component={TaskDetails}/>} />
+            <Route path="/task/new" element={<ProtectedRoute component={TaskForm}/>} />
             <Route path="/auth" element={<AuthPage />} />
+            <Route path='*' element={<Navigate to={'/'}/>}/>
           </Routes>
         </BrowserRouter>
       </TaskContextProvider>
--- app/src/components/AuthPage.tsx
+++ app/src/components/AuthPage.tsx
@@ -1,20 +1,25 @@
-import { useAuth0 } from "@auth0/auth0-react"
+import { useAuth0 } from '@auth0/auth0-react';
 
 const AuthPage: React.FC = () => {
-  const { loginWithRedirect, logout, isAuthenticated, user} = useAuth0()
-  
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
   return (
     <div>
-        {isAuthenticated ? (
-            <>
-               <h1>Welcome, {user?.name || 'User'}!</h1>
-               <button onClick={() => logout({ logoutParams: {returnTo: window.location.origin} })}>Logout</button>  
-            </>
-        ) : (
-            <button onClick={() => loginWithRedirect()}>Login</button>
-          )
-        }
+      {isAuthenticated ? (
+        <>
+          <h1>Welcome, {user?.name || 'User'}!</h1>
+          <button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>Login</button>
+      )}
     </div>
-  )
-}
-export default AuthPage
+  );
+};
+export default AuthPage;
